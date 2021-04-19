3bacdf231b65d07bdbe047182d96e7f74f98f84c
Juan Carlos Aranda Alonso <sistemas@injectronic.com>
2021-04-19 15:33:30 -0600

    added gcloud

--- essentials-tools.sh
+++ essentials-tools.sh
@@ -9,7 +9,7 @@ curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install web development dependencies
-sudo npm install -g typescript --silent @angular/cli @ionic/cli
+sudo npm install -g typescript @angular/cli @ionic/cli firebase-tools
 
 # Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -24,6 +24,16 @@ sudo snap install postman
 
 # Install VsCode
 sudo snap install --classic code
+
+# Install Google Cloud SDK
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 
 # Install PyCharm
 # sudo snap install pycharm-community --classic
